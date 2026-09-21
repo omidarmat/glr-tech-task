8ca5030ccb8c0b440a9d9a40a42d34cb730c0c57
Omid Armat <omidarmat@gmail.com>
2026-09-21 14:24:42 +0330

    chore: add back button in order steps

--- src/components/features/order-flow/OrderFlow.component.tsx
+++ src/components/features/order-flow/OrderFlow.component.tsx
@@ -5,6 +5,7 @@ import { FlowStepper, SelectedCustomer } from "./common";
 import { CustomerSelection } from "./customer-selection/CustomerSelection.component";
 import { OrderSummary } from "./order-summary/OrderSummary.component";
 import { ProductSelection } from "./product-selection/ProductSelection.component";
+import { ArrowLeftOutlined } from "@ant-design/icons";
 
 export function OrderFlow() {
   const { customer } = useOrderCustomer();
@@ -21,9 +22,26 @@ export function OrderFlow() {
     setStepId(OrderFlowStep.OrderSummary);
   }, []);
 
+  function goToPreviousStep() {
+    if (stepId === OrderFlowStep.ProductSelection) {
+      setStepId(OrderFlowStep.CustomerSelection);
+    } else if (stepId === OrderFlowStep.OrderSummary) {
+      setStepId(OrderFlowStep.ProductSelection);
+    }
+  }
+
   return (
     <div>
       <FlowStepper currentStepId={stepId} />
+      {stepId !== OrderFlowStep.CustomerSelection && (
+        <button
+          onClick={goToPreviousStep}
+          className="flex items-center gap-2 hover:bg-neutral-50 rounded-lg px-2 py-0.5 mb-2"
+        >
+          <ArrowLeftOutlined />
+          <span>Step back</span>
+        </button>
+      )}
       <SelectedCustomer customer={customer} />
 
       {stepId === OrderFlowStep.CustomerSelection && (
